6faee9cca1aade6c7106ba69bf9ab7e5452dacae
arey94 <reyesalex1337@yahoo.com>
2025-11-11 15:53:27 -0500

    Create transactionV2.js
    Added additional information in the transaction schema

---
+++ server/models/transactionV2.js
@@ -0,0 +1,65 @@
+// Transaction schema
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+
+  // Individuals involved in transaction
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+
+  // Transaction data
+  transactionId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  idempotencyKey: {           // prevents duplicate submissions
+    type: String,
+    index: true,
+  },
+
+  // Transaction amounts and currencies
+  baseCurrency:  {            // Currency sent by sender
+    type: String,
+    required: true
+  },
+  baseAmount: {               // Amount of currency sent
+    type: Number,
+    required: true,
+  },
+  quoteCurrency:  {            // Currency sent by receiver
+    type: String,
+    required: true
+  },
+  quoteAmount: {               // Amount of currency sent by receiver
+    type: Number,
+    required: true,
+  },
+  fxRate: {                   // Exchange rate
+    type: Number,
+    required: true,
+  },
+
+  // Transaction status information
+  status:{
+    type: String,
+    enum: ['pending', 'processing', 'successful', 'failed', 'cancelled'],
+    default: 'pending',
+    index: true
+  },
+  
+},
+
+{ timestamps: true }        // Provides time of creation
+
+);
+
+module.exports = mongoose.model('Transaction', transactionSchema);
